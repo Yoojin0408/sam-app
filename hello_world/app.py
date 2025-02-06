a9b95c365f904dae0f6618606c470b874527f979
Yoojin <yoojin.c48@gmail.com>
2025-02-06 04:20:34 +0000

    Changed return message

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello my friend",
+            "message": "Using canary deployments",
             # "location": ip.text.replace("\n", "")
         }),
     }
